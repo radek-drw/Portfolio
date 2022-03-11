2bac86c58942eaaa57c02e01b7ce60edabbbb872
Radek Drweski <rdrweski@gmail.com>
2022-03-11 23:46:29 +0000

    Change code to mobile first

--- js/typing.js
+++ js/typing.js
@@ -2,7 +2,7 @@ const typingBox = document.querySelector('.header__typing');
 let sentenceIndex = 0;
 let textIndex = 0;
 let oldTime = 0;
-const speed = 50; // Bigger value = slower typing animation
+const speed = 40; // Bigger value = slower typing animation
 let activeDOMelement = typingBox;
 
 const text = ["Hi! My name is Radek", "I'm passionate about all things JavaScript", "I work hard, I care about writing clean code and I genuinely love to learn", "I'm currently looking for the right oportunity to work in an environment that will help me progress into a full-stack role"];
