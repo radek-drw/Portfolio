81ebddf5a45093150d11f0d0848e4fcb869e0be9
Radek Drweski <rdrweski@gmail.com>
2022-03-17 11:03:02 +0000

    Add more funcionality to menu

--- js/hamburger.js
+++ js/hamburger.js
@@ -1,10 +1,17 @@
-const hamburgerMenu = document.querySelector('.header__menu-hamburger');
+const burgerMenu = document.querySelector('.header__menu-hamburger');
 const headerMenu = document.querySelector('.header__menu');
 const header = document.querySelector('.header');
 const blurryBg = document.querySelector('.header__blurry-bg');
 
-hamburgerMenu.addEventListener('click', () => {
-  hamburgerMenu.classList.toggle('open');
+burgerMenu.addEventListener('click', () => {
+  burgerMenu.classList.toggle('open');
   headerMenu.classList.toggle('show');
-  blurryBg.classList.toggle('blur')
+  blurryBg.classList.toggle('blur');
 });
+
+// Click blur background (when menu is open) to hide menu, change burger icon and remove blur
+blurryBg.addEventListener('click', () => {
+  burgerMenu.classList.remove('open');
+  headerMenu.classList.remove('show');
+  blurryBg.classList.remove('blur');
+})
