dcaffbf93c2f5b7f208af4484970a5f16d228e90
Radek Drweski <rdrweski@gmail.com>
2024-05-20 13:44:15 +0100

    add blur effect to background

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -1,22 +1,33 @@
-const mobileToggle = document.querySelector(".mobile-nav-toggle");
+const mobileToggle = document.querySelector(".navbar__mobile-nav-toggle");
 const mobileNav = document.querySelector(".navbar__mobile-nav");
 const elementsToBlur = document.querySelectorAll(
-  ".about, .skills, .portfolio, .contact, .footer"
+  ".navbar__name, .about, .skills, .portfolio, .contact, .footer"
 ); // Add all sections or elements you want to blur
+
+const toggleLines = Array.from(mobileToggle.querySelectorAll(".toggle__line"));
 
 const applyBlurEffect = (apply) => {
   elementsToBlur.forEach((element) => {
-    if (apply) {
-      element.classList.add("blur");
-    } else {
-      element.classList.remove("blur");
-    }
+    element.classList.toggle("blur", apply);
   });
 };
 
+const toggleNavActiveState = (isActive) => {
+  mobileNav.classList.toggle("navbar__mobile-nav--active", isActive);
+  mobileToggle.classList.toggle("navbar__mobile-nav-toggle--active", isActive);
+  toggleLines.forEach((line) => {
+    ["top", "middle", "bottom"].forEach((position) => {
+      if (line.classList.contains(`toggle__line-${position}`)) {
+        line.classList.toggle(`toggle__line-${position}--active`, isActive);
+      }
+    });
+  });
+  applyBlurEffect(isActive);
+};
+
 mobileToggle.addEventListener("click", () => {
-  mobileNav.classList.toggle("navbar__mobile-nav--active");
-  applyBlurEffect(mobileNav.classList.contains("navbar__mobile-nav--active"));
+  const isActive = !mobileNav.classList.contains("navbar__mobile-nav--active");
+  toggleNavActiveState(isActive);
 });
 
 document.addEventListener("click", (event) => {
@@ -25,15 +36,13 @@ document.addEventListener("click", (event) => {
     !mobileToggle.contains(event.target)
   ) {
     if (mobileNav.classList.contains("navbar__mobile-nav--active")) {
-      mobileNav.classList.remove("navbar__mobile-nav--active");
-      applyBlurEffect(false);
+      toggleNavActiveState(false);
     }
   }
 });
 
 mobileNav.addEventListener("click", (event) => {
   if (event.target.closest(".navbar__mobile-nav-item")) {
-    mobileNav.classList.remove("navbar__mobile-nav--active");
-    applyBlurEffect(false);
+    toggleNavActiveState(false);
   }
 });
