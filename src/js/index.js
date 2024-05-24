f7e6f37c347f9ec45ef2ca6ad97b603717e4d7e2
Radek Drweski <rdrweski@gmail.com>
2024-05-24 04:34:50 +0100

    return everything to stage before webpack

--- src/js/index.js
+++
@@ -1,6 +0,0 @@
-import "./header-animation.js";
-import "./navbar-links-animation.js";
-import "./navbar-links-scroll.js";
-import "./skills-slider.js";
-import "./form-validation.js";
-import "./mobile-menu.js";
