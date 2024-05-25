0ad31dc8942e80666b32225376b553c4c9d5403f
Radek Drweski <rdrweski@gmail.com>
2024-05-25 02:36:14 +0100

    add webpack and configure

---
+++ src/index.js
@@ -0,0 +1,8 @@
+import "./sass/index.sass";
+
+import "./js/header-animation.js";
+import "./js/navbar-links-animation.js";
+import "./js/navbar-links-scroll.js";
+import "./js/skills-slider.js";
+import "./js/form-validation.js";
+import "./js/mobile-menu.js";
---
+++ webpack.config.prod.js
@@ -0,0 +1,40 @@
+const path = require("path");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CopyWebpackPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  mode: "production",
+  entry: {
+    main: "./src/index.js",
+  },
+  output: {
+    filename: "[name]-[contenthash:4].js",
+    path: path.resolve(__dirname, "dist"),
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader"],
+      },
+      {
+        test: /\.(sass|scss)$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: "./src/index.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name]-[contenthash:4].css",
+    }),
+    new CopyWebpackPlugin({
+      patterns: [{ from: "./src/assets", to: "./assets" }],
+    }),
+  ],
+};
