d5527111aeb15af59efc840488ffeacddb6dbf24
radek-drw <rdrweski@gmail.com>
2025-09-16 09:25:36 +0100

    refactor: remove duplicate reCAPTCHA execution code

--- frontend/src/js/recaptcha.js
+++
@@ -1,7 +0,0 @@
-grecaptcha.ready(function () {
-  grecaptcha
-    .execute("""", { action: "submit" })
-    .then(function (token) {
-      document.getElementById("recaptchaToken").value = token;
-    });
-});
