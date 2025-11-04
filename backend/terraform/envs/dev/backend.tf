dc82becf45f0fe8c8c6941550f5ef61d86d68a28
radek-drw <rdrweski@gmail.com>
2025-11-04 14:03:20 +0000

    feat(terraform): configure S3 backend for dev environment state

---
+++ backend/terraform/envs/dev/backend.tf
@@ -0,0 +1,13 @@
+# ⚠️ WARNING: This backend assumes that the S3 bucket and DynamoDB table already exist.
+# If creating a new project, first go to the `backend-setup/` folder
+# and run `terraform init && terraform apply`.
+
+terraform {
+  backend "s3" {
+    bucket         = "radek-portfolio-terraform-state"
+    key            = "dev/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks-table"
+    encrypt        = true
+  }
+}
