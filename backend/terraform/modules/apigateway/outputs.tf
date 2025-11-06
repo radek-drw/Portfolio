de3ccb52d59228200571e8346a48c7bc2249998b
radek-drw <rdrweski@gmail.com>
2025-11-06 14:13:52 +0000

    feat(terraform): update api_url output to include api_path variable

--- backend/terraform/modules/apigateway/outputs.tf
+++ backend/terraform/modules/apigateway/outputs.tf
@@ -1,4 +1,8 @@
+variable "api_path" {
+  default = "contact"
+}
+
 output "api_url" {
-  description = "Deployed API Gateway URL"
-  value       = "${aws_apigatewayv2_api.contact_api.api_endpoint}/${aws_apigatewayv2_stage.stage.name}"
+  description = "Full API endpoint"
+  value       = "${aws_apigatewayv2_api.contact_api.api_endpoint}/${aws_apigatewayv2_stage.stage.name}/${var.api_path}"
 }
