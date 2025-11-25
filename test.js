bd55d8803840a448d44b0369ee6b0038ea8f0420
radek-drw <rdrweski@gmail.com>
2025-11-25 09:24:00 +0000

    chore: remove test file with fake secrets

--- test.js
+++
@@ -1,6 +0,0 @@
-const AWSSecretKey = 'wJalrXUtnFEMI/K7MDENG/bPxRfiCYEXAMPLEKEY';
-
-const GoogleApiKey = 'AIzaSyA-1234567890abcdefghijklmnOPQRSTU';
-
-console.log('AWS Secret Key:', AWSSecretKey);
-console.log('Google API Key:', GoogleApiKey);
