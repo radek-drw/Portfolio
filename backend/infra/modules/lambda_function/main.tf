9f14e07601ed57e267b253782ff106ec3e03e5d6
Radek Drweski <rdrweski@gmail.com>
2025-12-11 16:12:11 +0000

    chore: add AWS Lambda function resource configuration

---
+++ backend/infra/modules/lambda_function/main.tf
@@ -0,0 +1,13 @@
+resource "aws_lambda_function" "lambda" {
+  function_name    = "${var.env_name}-${var.lambda_name}"
+  handler          = var.handler
+  runtime          = var.runtime
+  timeout          = var.timeout
+  role             = var.role_arn
+  filename         = var.lambda_zip_path
+  source_code_hash = filebase64sha256(var.lambda_zip_path)
+
+  environment {
+    variables = var.environment_variables
+  }
+}
