a61add5d05e9bd73749a828c9b6d96cdc299822a
radek-drw <rdrweski@gmail.com>
2025-12-15 13:10:21 +0000

    refactor(apigateway): make route and CORS methods configurable

--- backend/infra/envs/dev/contact-form.apigateway.tf
+++ backend/infra/envs/dev/contact-form.apigateway.tf
@@ -3,5 +3,7 @@ module "contact_form_api_gateway" {
   env_name          = "dev"
   lambda_name       = module.contact_form_lambda.lambda_name
   lambda_invoke_arn = module.contact_form_lambda.lambda_arn
+  route_key         = "POST /contact"
   allow_origins     = ["http://localhost:9000"]
+  allow_methods     = ["OPTIONS", "POST"]
 }
