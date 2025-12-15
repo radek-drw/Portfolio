6eeb0f034071486425ab307f025e0b1b820d1e90
radek-drw <rdrweski@gmail.com>
2025-12-15 13:11:37 +0000

    refactor(apigateway): add route_key and allow_methods variables

--- backend/infra/modules/apigateway/variables.tf
+++ backend/infra/modules/apigateway/variables.tf
@@ -4,3 +4,10 @@ variable "lambda_invoke_arn" {}
 variable "allow_origins" {
   type = list(string)
 }
+variable "route_key" {
+  type = string
+}
+variable "allow_methods" {
+  type    = list(string)
+  default = ["OPTIONS"]
+}
