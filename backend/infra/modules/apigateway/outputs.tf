0c9a2d3a66b92cebddc0dfb48598fe238144b2aa
radek-drw <rdrweski@gmail.com>
2025-12-16 13:02:25 +0000

    remove unused file

--- backend/infra/modules/apigateway/outputs.tf
+++
@@ -1,8 +0,0 @@
-variable "api_path" {
-  default = "contact"
-}
-
-output "api_url" {
-  description = "Full API endpoint"
-  value       = "${aws_apigatewayv2_api.contact_api.api_endpoint}/${aws_apigatewayv2_stage.stage.name}/${var.api_path}"
-}
