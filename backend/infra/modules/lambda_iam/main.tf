930fcc38594378150995fe7ae1956a59784cbbee
Radek Drweski <rdrweski@gmail.com>
2025-12-16 18:23:34 +0000

    fix(iam): include env name in IAM role resource name

--- backend/infra/modules/lambda_iam/main.tf
+++ backend/infra/modules/lambda_iam/main.tf
@@ -1,5 +1,5 @@
 resource "aws_iam_role" "lambda_role" {
-  name = "${var.env_name}-${lambda_name}-role"
+  name = "${var.env_name}-${var.lambda_name}-role"
 
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
