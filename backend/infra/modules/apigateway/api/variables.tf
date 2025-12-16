c9a05e2d4f70547453523eaaa4e1be55983f3764
radek-drw <rdrweski@gmail.com>
2025-12-16 12:59:18 +0000

    chore(terraform): remove unused variables

--- backend/infra/modules/apigateway/api/variables.tf
+++ backend/infra/modules/apigateway/api/variables.tf
@@ -1,8 +1,5 @@
 variable "env_name" { type = string }
-variable "lambda_name" { type = string }
-variable "lambda_invoke_arn" { type = string }
 variable "allow_origins" { type = list(string) }
-variable "route_key" { type = string }
 variable "allow_methods" {
   type    = list(string)
   default = ["OPTIONS"]
---
+++ backend/infra/modules/apigateway/api_route/variables.tf
@@ -0,0 +1,5 @@
+variable "lambda_name" { type = string }
+variable "execution_arn" { type = string }
+variable "api_id" { type = string }
+variable "lambda_invoke_arn" { type = string }
+variable "route_key" { type = string }
