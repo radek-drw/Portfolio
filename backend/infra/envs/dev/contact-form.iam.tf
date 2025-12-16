3c90a1ed5d376ac83974e96db61547b8e6ba118c
radek-drw <rdrweski@gmail.com>
2025-12-16 16:56:02 +0000

    refactor(lambda-iam): make lambda_name and policy_description dynamic

--- backend/infra/envs/dev/contact-form.iam.tf
+++ backend/infra/envs/dev/contact-form.iam.tf
@@ -1,8 +1,8 @@
 module "contact_form_iam" {
   source             = "../../modules/lambda_iam"
   env_name           = "dev"
-  lambda_name        = "contact-form"
-  policy_description = "IAM policy for contact-form Lambda"
+  lambda_name        = module.contact_form_lambda.lambda_name
+  policy_description = "IAM policy for ${module.contact_form_lambda.lambda_name} Lambda"
   policy_document = {
     Version = "2012-10-17"
     Statement = [
