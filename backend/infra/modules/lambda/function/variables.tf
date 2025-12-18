5a1f3572472e2bc2bd91d8dd6344926491c90de1
radek-drw <rdrweski@gmail.com>
2025-12-18 15:55:55 +0000

    refactor(terraform): rename parent directory

--- backend/infra/modules/lambda/function/variables.tf
+++ backend/infra/modules/lambda/function/variables.tf
@@ -3,7 +3,7 @@ variable "lambda_name" {}
 variable "lambda_zip_path" {}
 variable "handler" { default = "index.handler" }
 variable "runtime" { default = "nodejs22.x" }
-variable "timeout" { default = 10 }
+variable "timeout" { default = 3 }
 variable "role_arn" {}
 variable "environment_variables" {
   type    = map(string)
