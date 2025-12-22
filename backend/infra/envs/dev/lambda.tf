eb29b6fd344cec94818163ed27285f183b7cf564
radek-drw <rdrweski@gmail.com>
2025-12-22 16:20:15 +0000

    feat(lambda): add description field to Lmabda module

--- backend/infra/envs/dev/lambda.tf
+++ backend/infra/envs/dev/lambda.tf
@@ -2,11 +2,12 @@ module "contact_form_lambda" {
   source          = "../../modules/lambda/function"
   env_name        = var.env_name
   lambda_name     = "contact-form"
-  lambda_zip_path = "../../../dist/sendContactForm.zip"
+  lambda_zip_path = "../../../dist/contact-form.zip"
   handler         = "index.handler"
   runtime         = "nodejs22.x"
   timeout         = 10
   role_arn        = module.contact_form_iam.role_arn
+  description     = "Handles contact form submissions. Validates inputs, verifies reCAPTCHA, and sends emails via SES"
   environment_variables = {
     RECAPTCHA_SECRET = data.aws_ssm_parameter.recaptcha_secret.value
     SES_FROM_ADDRESS = data.aws_ssm_parameter.ses_from_address.value
