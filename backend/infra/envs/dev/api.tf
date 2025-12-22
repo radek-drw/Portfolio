5b1901bffc33b362ff1dde51793dab419fc96161
radek-drw <rdrweski@gmail.com>
2025-12-22 16:18:10 +0000

    feat(api): add description field to API module

--- backend/infra/envs/dev/api.tf
+++ backend/infra/envs/dev/api.tf
@@ -3,4 +3,5 @@ module "api" {
   env_name      = var.env_name
   allow_origins = ["http://localhost:9000"]
   allow_methods = ["OPTIONS", "POST"]
+  description   = "Route for handling contact form submissions"
 }
