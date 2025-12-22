c22b482489749b60cdad82a62d7d6729d74a87e3
radek-drw <rdrweski@gmail.com>
2025-12-22 16:19:28 +0000

    refactor(iam): update description field in IAM module

--- backend/infra/envs/dev/iam.tf
+++ backend/infra/envs/dev/iam.tf
@@ -2,7 +2,7 @@ module "contact_form_iam" {
   source             = "../../modules/lambda/iam"
   env_name           = var.env_name
   lambda_name        = module.contact_form_lambda.lambda_name
-  policy_description = "IAM policy for ${module.contact_form_lambda.lambda_name} Lambda"
+  policy_description = "Allows Lambda to send emails (SES), write logs (CloudWatch), and read config from SSM"
   policy_document = {
     Version = "2012-10-17"
     Statement = [
@@ -17,9 +17,12 @@ module "contact_form_iam" {
         Resource = "arn:aws:logs:*:*:*"
       },
       {
-        Effect   = "Allow"
-        Action   = ["ssm:GetParameter", "ssm:GetParameters"]
-        Resource = "arn:aws:ssm:eu-west-1:*:parameter/*"
+        Effect = "Allow"
+        Action = ["ssm:GetParameter"]
+        Resource = [
+          "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/recaptcha_secret",
+          "arn:aws:ssm:${var.aws_region}:${data.aws_caller_identity.current.account_id}:parameter/ses-from-address"
+        ]
       }
     ]
   }
