9b7e5adc19e9e27b47db16472a7d9af5f2de5b1f
radek-drw <rdrweski@gmail.com>
2025-12-22 12:09:44 +0000

    refactor: replace hardcoded env_name with variable

--- backend/infra/envs/dev/api.tf
+++ backend/infra/envs/dev/api.tf
@@ -1,6 +1,6 @@
 module "api" {
   source        = "../../modules/apigateway/api"
-  env_name      = "dev"
+  env_name      = var.env_name
   allow_origins = ["http://localhost:9000"]
   allow_methods = ["OPTIONS", "POST"]
 }
