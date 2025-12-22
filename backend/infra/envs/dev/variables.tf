6c2bfdde31db1c9032eed0cdb8dd177e32f8097b
radek-drw <rdrweski@gmail.com>
2025-12-22 16:22:12 +0000

    chore: add env_name to eu-west-1

--- backend/infra/envs/dev/variables.tf
+++ backend/infra/envs/dev/variables.tf
@@ -2,3 +2,8 @@ variable "env_name" {
   type        = string
   description = "Environment name (dev, prod)"
 }
+
+variable "aws_region" {
+  type        = string
+  description = "AWS region to deploy resources in"
+}
