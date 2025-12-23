38d5cd44b33138efe53b677987df674b7e769f9a
radek-drw <rdrweski@gmail.com>
2025-12-23 11:50:00 +0000

    chore(terraform): update API Gateway description

--- backend/infra/envs/dev/api.tf
+++ backend/infra/envs/dev/api.tf
@@ -3,5 +3,5 @@ module "api" {
   env_name      = var.env_name
   allow_origins = ["http://localhost:9000"]
   allow_methods = ["OPTIONS", "POST"]
-  description   = "Route for handling contact form submissions"
+  description   = "API Gateway for ${var.env_name} environment"
 }
