fba26dd04274d840d882b26a5a0099c91cb5afda
radek-drw <rdrweski@gmail.com>
2025-12-26 16:08:49 +0000

    chore(terraform): update API Gateway CORS to use prod domains

--- backend/infra/envs/prod/api.tf
+++ backend/infra/envs/prod/api.tf
@@ -1,7 +1,7 @@
 module "api" {
   source        = "../../modules/apigateway/api"
   env_name      = var.env_name
-  allow_origins = ["http://localhost:9000"]
+  allow_origins = ["https://www.radek-drweski.com", "https://radek-drweski.com"]
   allow_methods = ["OPTIONS", "POST"]
   description   = "API Gateway for ${var.env_name} environment"
 }
