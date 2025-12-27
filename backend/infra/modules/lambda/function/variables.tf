30b4f388e9b2b5dbfda2aab45e29bbdc6be583fb
radek-drw <rdrweski@gmail.com>
2025-12-27 15:00:47 +0000

    feat(lambda): add typed variables for lambda module

--- backend/infra/modules/lambda/function/variables.tf
+++ backend/infra/modules/lambda/function/variables.tf
@@ -1,11 +1,20 @@
-variable "env_name" {}
-variable "lambda_name" {}
-variable "description" {}
-variable "lambda_zip_path" {}
-variable "handler" { default = "index.handler" }
-variable "runtime" { default = "nodejs22.x" }
-variable "timeout" { default = 3 }
-variable "role_arn" {}
+variable "env_name" { type = string }
+variable "lambda_name" { type = string }
+variable "description" { type = string }
+variable "lambda_zip_path" { type = string }
+variable "handler" {
+  type    = string
+  default = "index.handler"
+}
+variable "runtime" {
+  type    = string
+  default = "nodejs22.x"
+}
+variable "timeout" {
+  type    = number
+  default = 3
+}
+variable "role_arn" { type = string }
 variable "environment_variables" {
   type    = map(string)
   default = {}
