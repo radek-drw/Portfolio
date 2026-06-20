340510c95f64c9c4621d8958ff06d643579aac68
radek-drw <rdrweski@gmail.com>
2026-06-20 13:08:20 +0100

    refactor: rename contact route module to send email route and update source path

--- backend/infra/stacks/contact/routes.tf
+++ backend/infra/stacks/contact/routes.tf
@@ -1,8 +1,8 @@
-module "contact_route" {
-  source            = "../apigateway/route"
+module "send_email_route" {
+  source            = "../../modules/apigateway/route"
   api_id            = module.api.api_id
   execution_arn     = module.api.execution_arn
   route_key         = "POST /contact"
-  lambda_name       = module.contact_form_lambda.name
-  lambda_invoke_arn = module.contact_form_lambda.invoke_arn
+  lambda_name       = module.send_email_lambda.name
+  lambda_invoke_arn = module.send_email_lambda.invoke_arn
 }
