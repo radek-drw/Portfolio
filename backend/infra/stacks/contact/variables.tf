2c15b4672882a6eb6eb070f6043ee20712e10c41
radek-drw <rdrweski@gmail.com>
2026-06-20 02:10:55 +0100

    refactor: remove unused CORS origins variable from contact stack

--- backend/infra/stacks/contact/variables.tf
+++ backend/infra/stacks/contact/variables.tf
@@ -7,8 +7,3 @@ variable "aws_region" {
   description = "AWS region to deploy resources in"
   type        = string
 }
-
-variable "allow_origins" {
-  description = "List of allowed CORS origins for API Gateway"
-  type        = list(string)
-}
