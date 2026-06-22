66addc12255be120d94ad3a587e3f3fdc9882d91
radek-drw <rdrweski@gmail.com>
2026-06-22 01:38:40 +0100

    fix: update to use variable references for API ID and execution ARN

--- backend/infra/stacks/contact/routes.tf
+++ backend/infra/stacks/contact/routes.tf
@@ -1,7 +1,7 @@
 module "send_email_route" {
   source            = "../../modules/apigateway/route"
-  api_id            = module.api.api_id
-  execution_arn     = module.api.execution_arn
+  api_id            = var.api_id
+  execution_arn     = var.execution_arn
   route_key         = "POST /contact"
   lambda_name       = module.send_email_lambda.name
   lambda_invoke_arn = module.send_email_lambda.invoke_arn
