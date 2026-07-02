de8b087c9a4080c1347e4b66cac668cb46d7aee5
radek-drw <rdrweski@gmail.com>
2026-07-02 16:52:31 +0100

    fix: update resource names in API Gateway integration for consistency

--- backend/infra/modules/apigateway/route/main.tf
+++ backend/infra/modules/apigateway/route/main.tf
@@ -1,17 +1,17 @@
-resource "aws_apigatewayv2_integration" "integration" {
+resource "aws_apigatewayv2_integration" "this" {
   api_id                 = var.api_id
   integration_type       = "AWS_PROXY"
   integration_uri        = var.lambda_invoke_arn
   payload_format_version = "2.0"
 }
 
-resource "aws_apigatewayv2_route" "route" {
+resource "aws_apigatewayv2_route" "this" {
   api_id    = var.api_id
   route_key = var.route_key
-  target    = "integrations/${aws_apigatewayv2_integration.integration.id}"
+  target    = "integrations/${aws_apigatewayv2_integration.this.id}"
 }
 
-resource "aws_lambda_permission" "permission" {
+resource "aws_lambda_permission" "this" {
   statement_id  = "AllowAPIGatewayInvoke-${replace(replace(var.route_key, " ", "-"), "/", "-")}"
   action        = "lambda:InvokeFunction"
   function_name = var.lambda_name
