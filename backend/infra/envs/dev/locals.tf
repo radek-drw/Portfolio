ae1ce0cb4533288fd7d3e3e40fc956eb0e60028a
radek-drw <rdrweski@gmail.com>
2026-07-08 05:32:20 +0100

    refactor: remove unused aws_region local

--- backend/infra/envs/dev/locals.tf
+++ backend/infra/envs/dev/locals.tf
@@ -1,13 +1,14 @@
 locals {
   env_name      = "dev"
-  aws_region    = "eu-west-1"
   allow_origins = ["http://localhost:9000"]
-  branch_name    = "dev"
-  bucket_name    = "dev-portfolio-static-site"
+  branch_name   = "dev"
+  bucket_name   = "dev-portfolio-static-site"
+  project_name  = "portfolio"
+  domain_name   = "dev.radek-drweski.com"
   tags = {
     Environment = "dev"
     Project     = "portfolio"
     ManagedBy   = "Terraform"
   }
-  project_name = "portfolio"
+
 }
