dcafc9b0c427fc1d71622252f403139b0b034780
radek-drw <rdrweski@gmail.com>
2026-07-09 04:49:17 +0100

    refactor: move SSM parameter data sources to lambda stack

--- backend/infra/stacks/backend/contact/lambda.tf
+++ backend/infra/stacks/backend/contact/lambda.tf
@@ -1,3 +1,13 @@
+data "aws_ssm_parameter" "recaptcha_secret" {
+  name            = "recaptcha-secret"
+  with_decryption = true
+}
+
+data "aws_ssm_parameter" "ses_from_address" {
+  name            = "ses-from-address"
+  with_decryption = true
+}
+
 module "send_email_lambda" {
   source          = "../../modules/lambda/function"
   env_name        = var.env_name
