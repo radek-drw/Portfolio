b7508d81df5d2bcc695693fc9271c0de18a27388
radek-drw <rdrweski@gmail.com>
2026-07-10 05:16:19 +0100

    refactor: relocate infrastructure directory to repository root

--- backend/infra/envs/prod/main.tf
+++
@@ -1,7 +0,0 @@
-module "backend" {
-  source = "../../modules/backend"
-
-  aws_region    = "eu-west-1"
-  env_name      = "prod"
-  allow_origins = ["https://www.radek-drweski.com", "https://radek-drweski.com"]
-}
