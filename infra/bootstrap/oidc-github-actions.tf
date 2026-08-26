08acca725c21575d1d72f1e46321c0e179a0b5f0
radek-drw <rdrweski@gmail.com>
2026-08-26 05:06:18 +0100

    feat(terraform): add GitHub Actions OIDC provider configuration

--- infra/bootstrap/oidc-github-actions.tf
+++ infra/bootstrap/oidc-github-actions.tf
@@ -1,6 +1,19 @@
 # GitHub Actions authentication via OIDC
 # Allows CI/CD workflows to deploy Lambda functions
 
+resource "aws_iam_openid_connect_provider" "github" {
+  url = "https://token.actions.githubusercontent.com"
+
+  client_id_list = [
+    "sts.amazonaws.com"
+  ]
+
+  thumbprint_list = [
+    "6938fd4d98bab03faadb97b34396831e3780aea1"
+  ]
+}
+
+
 locals {
   environments = {
     dev = {
