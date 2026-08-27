a0710a34a69204b4c389f9ab63d2796e86dd28c0
radek-drw <rdrweski@gmail.com>
2026-08-27 09:13:15 +0100

    chore(backend): remove redundant success message from email response

--- backend/src/send-email.js
+++ backend/src/send-email.js
@@ -115,7 +115,6 @@ export const handler = async (event) => {
       statusCode: 200,
       body: JSON.stringify({
         success: true,
-        message: 'Email sent successfully. Thank you for reaching out!!',
       }),
     };
   } catch (err) {
