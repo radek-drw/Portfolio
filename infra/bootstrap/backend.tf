5c6e3cf0a0ba23492cd1290cfcebeffd762b0c20
radek-drw <rdrweski@gmail.com>
2026-09-01 16:21:53 +0100

    refactor(terraform): migrate backend locking to S3 lockfile

--- infra/bootstrap/backend.tf
+++ infra/bootstrap/backend.tf
@@ -3,7 +3,7 @@ terraform {
     bucket         = "radek-portfolio-terraform-state"
     key            = "bootstrap/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "terraform-locks-table"
+    use_lockfile   = true
     encrypt        = true
   }
 }
--- infra/envs/dev/backend.tf
+++ infra/envs/dev/backend.tf
@@ -8,7 +8,7 @@ terraform {
     bucket         = "radek-portfolio-terraform-state"
     key            = "dev/terraform.tfstate"
     region         = "eu-west-1"
-    dynamodb_table = "terraform-locks-table"
+    use_lockfile   = true
     encrypt        = true
   }
 }
