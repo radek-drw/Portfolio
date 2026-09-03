cc8e8324923d7bb619c8aef173a7f79089933a51
radek-drw <rdrweski@gmail.com>
2026-09-03 10:50:32 +0100

    refactor(terraform): simplify route module naming

--- infra/stacks/backend/contact-form/routes.tf
+++ infra/stacks/backend/contact-form/routes.tf
@@ -1,8 +1,8 @@
-module "send_email_route" {
+module "route" {
   source            = "../../../modules/backend/apigateway/route"
   api_id            = var.api_id
   execution_arn     = var.execution_arn
   route_key         = "POST /contact"
-  lambda_name       = module.send_email_lambda.name
-  lambda_invoke_arn = module.send_email_lambda.invoke_arn
+  lambda_name       = module.lambda.name
+  lambda_invoke_arn = module.lambda.invoke_arn
 }
