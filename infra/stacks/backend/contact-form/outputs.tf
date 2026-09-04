e6f0b387b92caa46ed675dc9697d251c266ff54e
radek-drw <rdrweski@gmail.com>
2026-09-04 09:17:47 +0100

    refactor(terraform): expose lambda ARN

--- infra/stacks/backend/contact-form/outputs.tf
+++ infra/stacks/backend/contact-form/outputs.tf
@@ -2,3 +2,8 @@ output "contact_api_url" {
   description = "Endpoint used by the frontend to submit contact form data"
   value       = "${var.api_url}/contact"
 }
+
+output "lambda_arn" {
+  description = "ARN of the Lambda function"
+  value       = module.lambda.arn
+}
