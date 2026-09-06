3e62bcc46b75ef2fcac5f47d0d59cf49e4dd89d4
radek-drw <rdrweski@gmail.com>
2026-09-06 13:02:23 +0100

    test(lambda): add temporary lambda for deployment workflow

--- backend/src/hello-world.js
+++ backend/src/hello-world.js
@@ -2,7 +2,7 @@ export const handler = async () => {
   return {
     statusCode: 200,
     body: JSON.stringify({
-      message: 'Hello from test Lambda!',
+      message: 'Hello from test Lambda!!!!',
     }),
   };
 };
