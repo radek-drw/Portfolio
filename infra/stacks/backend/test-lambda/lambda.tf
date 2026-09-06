2cdd1ff8f807c1f384f250beb489a148358bae0d
radek-drw <rdrweski@gmail.com>
2026-09-06 11:34:40 +0100

    test(lambda): add temporary lambda for deployment workflow

--- infra/stacks/backend/test-lambda/lambda.tf
+++ infra/stacks/backend/test-lambda/lambda.tf
@@ -1,8 +1,8 @@
 module "lambda" {
   source          = "../../../modules/backend/lambda/function"
   env_name        = var.env_name
-  lambda_name     = "test-lambda"
-  lambda_zip_path = "${path.root}/../../../backend/dist/test-lambda.zip"
+  lambda_name     = "hello-world"
+  lambda_zip_path = "${path.root}/../../../backend/dist/hello-world.zip"
   handler         = "index.handler"
   role_arn        = ""
   description     = "test"
