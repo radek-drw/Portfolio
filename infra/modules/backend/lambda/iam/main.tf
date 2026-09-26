2496df06ad3b994e0f5f6c6380e8f6ec7679b91a
radek-drw <rdrweski@gmail.com>
2026-09-26 16:58:39 +0100

    test

--- infra/modules/backend/lambda/iam/main.tf
+++ infra/modules/backend/lambda/iam/main.tf
@@ -4,6 +4,7 @@ resource "aws_iam_role" "this" {
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [{
+      Sid       = "LambdaAssumeRole"
       Effect    = "Allow"
       Principal = { Service = "lambda.amazonaws.com" }
       Action    = "sts:AssumeRole"
